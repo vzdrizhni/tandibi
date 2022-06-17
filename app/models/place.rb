be4bea816d04fa3ebc2ae57edc7af3cc2ce6bbe0
Roman <vzdrizhni@gmail.com>
2022-06-17 21:11:04 +0500

    Add new specs and models validation

--- app/models/place.rb
+++ app/models/place.rb
@@ -17,4 +17,13 @@
 #  index_places_on_locale_and_coordinate  (locale,coordinate) UNIQUE
 #
 class Place < ApplicationRecord
+  PLACE_TYPES = [
+      "restaurant",
+      "coffee_shop",
+      "mall",
+      "hotel",
+      "other",
+  ].freeze
+
+  validates :place_type, inclusion: {in: PLACE_TYPES}
 end
--- app/models/sight.rb
+++ app/models/sight.rb
@@ -17,5 +17,12 @@
 #  fk_rails_...  (place_id => places.id)
 #
 class Sight < ApplicationRecord
+  ACTIVITY_TYPES = [
+      CHECKIN = "checkin",
+      CHECKOUT = "checkout"
+  ].freeze
+
   belongs_to :place
+
+  validates :activity_type, presence: true, inclusion: { in: ACTIVITY_TYPES }
 end
